b6a216fa03de503ac5de29c7851d09139eeddd75
carriesmith <carrie.elizabeth@gmail.com>
2015-09-30 12:26:28 -0400

    middleware log request method

--- server.js
+++ server.js
@@ -20,6 +20,14 @@ adminRouter.get('/', function(req, res){
   res.send('I am the dashboard');
 });
 
+// route middleware that will happen on every request
+adminRouter.use(function(req, res, next){
+  // log each request to the console
+  console.log(req.method, req.url);
+  // continue doing what we were doing and go to the route
+  next();
+});
+
 // users page (http://localhost:1337/admin/users
 adminRouter.get('/users', function(req, res){
   res.send('I show all the users');
